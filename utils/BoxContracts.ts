266e73d55d37cec2d906917818617d05096c65f6
Goblin Saga <167577467+goblinsaga@users.noreply.github.com>
2025-02-26 17:02:42 -0600

    Update BoxContracts.ts

--- utils/BoxContracts.ts
+++ utils/BoxContracts.ts
@@ -4,8 +4,8 @@ import { getContract } from "thirdweb";
 import { stakingABI } from "./MiningsABI";
 
 const nftContractAddress = "0xE62b57bA772DFf6Aa044407D79B4B12fA28a4942";
-const rewardTokenContractAddress = "0x8a5135FFb75af2460605d26c190DB5862c6Ec55b";
-const stakingContractAddress = "0x997C6fb85EE4E54Fe099A1C83Cd9f53b1BceF65C";
+const rewardTokenContractAddress = "0xC3882D10e49Ac4E9888D0C594DB723fC9cE95468";
+const stakingContractAddress = "0x7bBF62c83d3d00eCFDA3Ea98355B2895A453d786";
 
 export const NFT_CONTRACT = getContract({
     client: client,
