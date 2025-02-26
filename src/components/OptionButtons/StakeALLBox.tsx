9406460d7f9e9f2809473efc4c09dce32281a502
Goblin Saga <167577467+goblinsaga@users.noreply.github.com>
2025-02-26 16:26:30 -0600

    Update StakeALLBox.tsx

--- src/components/OptionButtons/StakeALLBox.tsx
+++ src/components/OptionButtons/StakeALLBox.tsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { useAddress, useContract, useContractRead } from "@thirdweb-dev/react";
 import { ethers } from "ethers";
-import { STAKING_CONTRACT_ADDRESS } from "../../../consts/contracts2New";
+import { STAKING_CONTRACT_ADDRESS, BUSINESSES_CONTRACT_ADDRESS2 } from "../../../consts/contracts2New";
 import SuccessMessagePopup from "../popups/SuccessMessagePopup";
 import ErrorMessagePopup from "../popups/ErrorMessagePopup";
-import StakingContractABI from "../../../contracts/StakingContractBoxABI.json"; // Asegúrate de tener el ABI correcto
+import StakingContractABI from "../../../contracts/StakingContractBoxABI.json"; // ABI del contrato de staking
 
 const StakeAllBox: React.FC = () => {
     const [errorMessage, setErrorMessage] = useState('');
@@ -50,19 +50,30 @@ const StakeAllBox: React.FC = () => {
             const provider = new ethers.providers.Web3Provider(window.ethereum);
             await provider.send("eth_requestAccounts", []); // Solicita conexión de la billetera
             const signer = provider.getSigner();
-            const stakingContract = new ethers.Contract(STAKING_CONTRACT_ADDRESS, StakingContractABI, signer);
 
-            // Agrupando las transacciones para que se procesen juntas
+            // Crear una instancia del contrato ERC1155 usando ethers.js
+            const erc1155 = new ethers.Contract(
+                BUSINESSES_CONTRACT_ADDRESS2,
+                [
+                    "function setApprovalForAll(address operator, bool approved)",
+                ],
+                signer
+            );
+
+            // Aprobar todos los tokens ERC1155 para el contrato de staking
+            const approveTx = await erc1155.setApprovalForAll(STAKING_CONTRACT_ADDRESS, true);
+            await approveTx.wait(); // Esperar a que la transacción se confirme
+
+            // Hacer stake de todos los NFTs
             const txPromises = stakedTokenIds.map((tokenId, i) => {
                 const amount = stakedTokenAmounts[i];
-                // Hacer stake de todos los tokens en una transacción combinada
                 return stakingContract.stake(tokenId, amount);
             });
 
-            // Espera todas las transacciones en paralelo
+            // Esperar todas las transacciones en paralelo
             const txResults = await Promise.all(txPromises);
-            
-            // Espera a que todas las transacciones se confirmen
+
+            // Esperar a que todas las transacciones se confirmen
             await Promise.all(txResults.map(tx => tx.wait()));
 
             setSuccessMessage("All NFTs Staked Successfully");
