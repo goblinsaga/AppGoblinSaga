4c39dd57b7b52703146da10d3cd38466904f612a
Goblin Saga <167577467+goblinsaga@users.noreply.github.com>
2025-03-11 09:18:35 -0600

    Update defi.js

--- pages/defi.js
+++ pages/defi.js
@@ -1,34 +1,20 @@
-import SEHeader2Two from "../src/components/SimpleEarnTwoTestTwo";
-import SEHeaderT from "../src/components/SimpleEarnOneTestTwo";
-import SEHeaderH from "../src/components/SimpleEarnOneTestThree";
-import SEHeaderThree from "../src/components/SimpleEarnThreeTest";
 import SectionDivider from "../src/components/SectionDivider";
 import Faq from "../src/components/Faq";
 import Layout from "../src/layout/Layout";
 import TokenSwap from "../src/components/TokenSwapGSA";
 import TokenSwapWGSA from "../src/components/TokenSwapWGSA";
+import StakeGSA from "../src/components/StakesGSA";
 
 const DeFi = () => {
     return (
         <Layout pageTitle={"DeFi"}>
             <TokenSwap />
-            {/* Section RoadMap */}
-            <SectionDivider />
-            {/* !Section RoadMap */}
-            <SEHeader2Two />
-            {/* Section Divider */}
-            <SectionDivider />
-
-            <SEHeaderThree />
-
-            <SectionDivider />
-            {/* Section Divider */}
-            <SEHeaderT />
-            <SEHeaderH />
 
             <SectionDivider />
 
             <TokenSwapWGSA />
+
+            <StakeGSA />
 
             <SectionDivider />
 
