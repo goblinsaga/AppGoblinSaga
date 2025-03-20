6207be14b7f85e8049c8ddfaa59bccfd4d3202a7
Goblin Saga <167577467+goblinsaga@users.noreply.github.com>
2025-03-20 01:16:49 -0600

    Update NFTCard3.tsx

--- src/components/NFTCard3.tsx
+++ src/components/NFTCard3.tsx
@@ -118,7 +118,7 @@ export default function NFTCardBox({ nft }: Props) {
     };
 
     const updateQuantity = (type: string) => {
-        if (type === "+" && quantity < 5) {
+        if (type === "+" && quantity < 15) {
             setQuantity(quantity + 1);
         } else if (type === "-" && quantity > 1) {
             setQuantity(quantity - 1);
