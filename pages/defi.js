4f48f5daf844c87d67d2fb32efa5cdacb57254f4
Goblin Saga <167577467+goblinsaga@users.noreply.github.com>
2025-03-26 16:04:47 -0600

    Update defi.js

--- pages/defi.js
+++ pages/defi.js
@@ -4,14 +4,11 @@ import Layout from "../src/layout/Layout";
 import NewUsersThree from "../src/components/NewUsers3";
 import TokenSwapWGSA from "../src/components/TokenSwapWGSA";
 import StakeGSA from "../src/components/StakesGSA";
-import Announce from "../src/components/Alerts";
 
 const DeFi = () => {
     return (
         <Layout pageTitle={"DeFi"}>
             <NewUsersThree />
-
-            <Announce />
 
             <TokenSwapWGSA />
 
