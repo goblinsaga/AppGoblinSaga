aa1b50733b9285ea43864b70f34f55a96b73e3ea
Goblin Saga <167577467+goblinsaga@users.noreply.github.com>
2025-04-19 17:30:02 -0600

    Update ZeusOwned.tsx

--- src/components/ZeusOwned.tsx
+++ src/components/ZeusOwned.tsx
@@ -1,25 +1,35 @@
-import React from 'react';
-import { useContract, useContractRead, useAddress } from '@thirdweb-dev/react';
-import { STAKING_CONTRACT_ADDRESS } from '../../consts/ZeusContracts';
+'use client';
 
-const ZeusMiningCount: React.FC = () => {
-  const address = useAddress();
-  const { contract } = useContract(STAKING_CONTRACT_ADDRESS);
-  const { data: stakedTokens } = useContractRead(contract, "getStakeInfo", [address]);
+import { useAddress, useContract, useContractRead } from "@thirdweb-dev/react";
+import { STAKING_CONTRACT_ADDRESS } from "../../consts/ZeusContracts";
+import { useEffect, useState } from "react";
 
-  // Función para contar el número de NFTs en el array stakedTokens
-  const getTotalStakedNfts = () => {
-    if (!stakedTokens || !stakedTokens[0]) return 0;
-    return stakedTokens[0].length;
-  };
-
-  return (
-    <div>
-      <p>
-        {getTotalStakedNfts()} Zeus
-      </p>
-    </div>
-  );
+type Props = {
+  tokenId: number;
 };
 
-export default ZeusMiningCount;
+export default function ZeusMiningCount({ tokenId }: Props) {
+  const address = useAddress();
+  const [stakedCount, setStakedCount] = useState<number>(0);
+
+  const { contract: stakingContract } = useContract(STAKING_CONTRACT_ADDRESS);
+  const { data: stakeInfo } = useContractRead(
+    stakingContract,
+    "getStakeInfoForToken",
+    address ? [tokenId, address] : undefined
+  );
+
+  useEffect(() => {
+    try {
+      if (stakeInfo && stakeInfo[0]) {
+        setStakedCount(stakeInfo[0].toNumber());
+      } else {
+        setStakedCount(0);
+      }
+    } catch {
+      setStakedCount(0);
+    }
+  }, [stakeInfo]);
+
+  return stakedCount > 0 ? <>{stakedCount}</> : null;
+}
